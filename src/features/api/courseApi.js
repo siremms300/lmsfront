8dee6c68358649dd9c08f01f115469524e89750b
Idoga Emmanuel <siremms300@gmail.com>
2024-12-31 05:09:24 -0800

    changed the cors

--- src/features/api/courseApi.js
+++ src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://lmsback.vercel.app/api/v1/course";
+const COURSE_API = "http://localhost:3000/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
--- src/pages/admin/lecture/LectureTab.jsx
+++ src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://lmsback.vercel.app/api/v1/media";
+const MEDIA_API = "http://localhost:3000/api/v1/media";
 
 const LectureTab = () => {
   const [lectureTitle, setLectureTitle] = useState("");
