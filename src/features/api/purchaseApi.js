7f8c07ad6a843afc18474a7bb86d53cc00cf0e1a
Idoga Emmanuel <siremms300@gmail.com>
2024-12-31 05:15:48 -0800

    changed the cors

--- src/features/api/purchaseApi.js
+++ src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:3000/api/v1/purchase";
+const COURSE_PURCHASE_API = "https://lmsback-x03j.onrender.com/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
--- src/pages/admin/lecture/LectureTab.jsx
+++ src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:3000/api/v1/media";
+const MEDIA_API = "https://lmsback-x03j.onrender.com/api/v1/media";
 
 const LectureTab = () => {
   const [lectureTitle, setLectureTitle] = useState("");
